62692723e6beb8b1f23c53cde6b1c6dcd3625b22
Nicholas Lizotte <lizottenj@gmail.com>
2015-07-06 17:45:09 -0400

    Add email button

--- app/src/main/java/it/trump/phonestatustester/MainActivityFragment.java
+++ app/src/main/java/it/trump/phonestatustester/MainActivityFragment.java
@@ -2,15 +2,18 @@ package it.trump.phonestatustester;
 
 import android.annotation.TargetApi;
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Build;
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.telephony.CellLocation;
 import android.telephony.TelephonyManager;
 import android.telephony.gsm.GsmCellLocation;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 /**
@@ -34,17 +37,33 @@ public class MainActivityFragment extends Fragment {
 
         TextView tv = (TextView) getActivity().findViewById(R.id.infoView);
 
-        TelephonyManager tMan = (TelephonyManager) getActivity().getSystemService(Context.TELEPHONY_SERVICE);
+        final String output = getTelephonyData();
 
-        tMan.getLine1Number();
+        tv.setText(output);
+
+        Button email = (Button) getActivity().findViewById(R.id.emailButton);
+        if(email != null) {
+            email.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    composeEmail(output);
+                }
+            });
+        }
+    }
+
+    private String getTelephonyData() {
+        TelephonyManager tMan = (TelephonyManager) getActivity().getSystemService(Context.TELEPHONY_SERVICE);
 
         String output = "";
 
         output = addKey(output, "Line 1 Number", tMan.getLine1Number());
         output = addKey(output, "Device software version", tMan.getDeviceSoftwareVersion());
-        output = addKey(output, "Group Id Level 1", tMan.getGroupIdLevel1());
-        output = addKey(output, "MMS UA Prof URL", tMan.getMmsUAProfUrl());
-        output = addKey(output, "MMS UA", tMan.getMmsUserAgent());
+        if(Build.VERSION.SDK_INT >= 19) {
+            output = addKey(output, "Group Id Level 1", tMan.getGroupIdLevel1());
+            output = addKey(output, "MMS UA Prof URL", tMan.getMmsUAProfUrl());
+            output = addKey(output, "MMS UA", tMan.getMmsUserAgent());
+        }
         output = addKey(output, "Network Country ISO", tMan.getNetworkCountryIso());
         output = addKey(output, "Network Operator", tMan.getNetworkOperator());
         output = addKey(output, "Network Operator Name", tMan.getNetworkOperatorName());
@@ -57,7 +76,7 @@ public class MainActivityFragment extends Fragment {
         output = addKey(output, "Voicemail number", tMan.getVoiceMailNumber());
         output = addKey(output, "Is Network Roaming", String.valueOf(tMan.isNetworkRoaming()));
 
-        if(Build.VERSION.SDK_INT >= 17) {
+        if(Build.VERSION.SDK_INT >= 21) {
             output = addKey(output, "Is SMS Enabled", String.valueOf(tMan.isSmsCapable()));
         }
 
@@ -68,13 +87,24 @@ public class MainActivityFragment extends Fragment {
             output = addKey(output, "GSM lac", String.valueOf(((GsmCellLocation) cl).getLac()));
             output = addKey(output, "GSM psc", String.valueOf(((GsmCellLocation) cl).getPsc()));
         }
-
-        tv.setText(output);
+        return output;
     }
 
     private String addKey(String output, String key, String value) {
 
         output += (key + ":\t" + value + "\n");
         return output;
+    }
+
+    public void composeEmail(String body) {
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:")); // only email apps should handle this
+        intent.putExtra(Intent.EXTRA_EMAIL, new String[]{"nick@trump.it"});
+        intent.putExtra(Intent.EXTRA_SUBJECT, "Telephony Data");
+        intent.putExtra(Intent.EXTRA_TEXT, body);
+
+        if (getActivity() != null && intent.resolveActivity(getActivity().getPackageManager()) != null) {
+            startActivity(intent);
+        }
     }
 }
